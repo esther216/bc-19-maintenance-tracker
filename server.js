dba8a2973fed6abae834047641404f7dd1b04bf1
Esther Falayi <aquaster193@gmail.com>
2016-12-06 09:41:16 +0100

    update dependencies

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// var http= require('http');
-
-// function handleRequests(req, res){
-// 	res.end('Hello World!');
-// }
-
-// var server= http.createServer(handleRequests);
-
-// server.listen(3000, function(){
-// 	console.log("Listening on port: 3000");
-// });
-
 var express= require('express');
 var app= express();
 var port= 3000;
