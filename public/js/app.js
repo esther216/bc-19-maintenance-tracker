3dfe55d3bbd938336e25724dfe036ffcff8d8388
Esther Falayi <aquaster193@gmail.com>
2016-12-08 04:53:47 +0100

    update: add new facilities and requests

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 (function(){
-
 	var config = {
 	  apiKey: "AIzaSyCic31NC__5P22dEgpoheCGHDm1AdzXRvw",
 	  authDomain: "maintenance-tracker-7a4ab.firebaseapp.com",
@@ -11,10 +10,86 @@
 
 	var db = firebase.database();
 	var usersRef= db.ref('users');
-
+	var facilityRef = db.ref('facilities');
+	var requestRef = db.ref('requests');
 
 	var newUser= {};
+	var newFacility = {};
+	var newRequest = {};
+
+	function generateID(){
+		var id = Math.random().toString(36).substring(2,7);
+		return id;
+	}
+
+	function addRow(table, cols){
+		var row = $('<tr/>');
+		for( var i = 0; i < cols.length; i++){
+			var col = $('<td/>').append(cols[i]);
+			row.append(col);
+		}
+		table.append(row);
+	}
+
+	function addOptions(select, options){
+		for( var i = 0; i < options.length; i++ ){
+			select.append($('<option>', {
+				value: i + 1,
+				text: options[i]
+			}));
+		}
+	}
+	
 	$(document).ready(function(){
+
+		function displayAllFacilities(){
+			var table = $('#all-facilities');
+			var select = $('#report > div > div > div > select');
+			var options = [];
+			facilityRef.once('value', function(snapshot){
+				
+				var obj = snapshot.val();
+				var i = 1;
+				for ( var key in obj ){
+
+					var data = [i];
+					data.push(obj[key].id);
+					data.push(obj[key].name);
+					options.push(obj[key].name);
+					data.push(obj[key].location)
+
+					addRow(table, data);
+
+					i++;
+				}
+				addOptions(select, options);
+			});
+			
+		}
+
+		function displayAllRequests(){
+			var table = $('#requests > table');
+			requestRef.once('value', function(snapshot){
+				var obj = snapshot.val();
+				var i = 1;
+				
+				for ( var key in obj ){
+					var data = [i];
+					data.push(obj[key].facility.name);
+					data.push(obj[key].description);
+					data.push(obj[key].approval);
+					data.push(obj[key].resolved);
+
+					addRow(table, data);
+					i++;
+				}
+			
+			});
+		}
+
+		displayAllFacilities();
+		displayAllRequests();
+
 		$('#sign-up-btn').click(function(){
 			newUser.name = $('.modal-body > div > #fname').val();
 			newUser.email = $('.modal-body > div > #email').val();
@@ -26,7 +101,7 @@
 			.then(function(user){
 				newUser.id = user.uid;
 				usersRef.push(newUser);
-				$('#sign-up').fadeOut(1000);
+				$(this).attr('data-dismiss', 'modal');
 			})
 			.catch(function(error){
 				console.log(error);
@@ -62,5 +137,42 @@
 		});
 	});
 
-	
+	// add a facility
+	$('#facility-btn').click(function(){
+		var table = $('#all-facilities');
+		var data = [];
+		var numberOfRows = $('#all-facilities > tbody > tr').length;
+		
+		console.log(numberOfRows);
+		data[0] = numberOfRows + 1;
+		newFacility.id = generateID();
+		newFacility.name = $('.modal-body > div > #facility-name').val();
+		newFacility.location = $('.modal-body > div > #facility-location').val();
+		
+		facilityRef.push(newFacility);
+		data.push(newFacility.id);
+		data.push(newFacility.name);
+		data.push(newFacility.location);
+		addRow(table, data);
+		$(this).attr('data-dismiss', 'modal');
+	});
+
+
+	// report a case / send a request
+	$('#report-btn').click(function(){
+		var selected = $('#report > div > div > div > select option:selected').text();
+		var description = $('#description').val();
+		var filter = facilityRef.orderByChild("name").equalTo(selected);
+		newRequest.approval = "awaiting";
+		newRequest.description = description;
+		newRequest.resolved = "";
+		
+		filter.on('value', function(snapshot){
+			snapshot.forEach(function(childSnapshot){
+				newRequest["facility"] = childSnapshot.val();
+				requestRef.push(newRequest);
+			});
+		});	
+	});
+
 })();
